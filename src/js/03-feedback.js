99b59d738dcae4f3b3051bd3361bbb570e1acc6c
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-18 13:37:20 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
+const dataDisplayEl = {`email: ${ formEl.email.value },
+    message: ${ formEl.message.value }`,
+};
+console.log(dataDisplayEl);
+
+// formEl.addEventListener('submit', handleDisplayReport);
+
+// function handleDisplayReport(e) {
+//   e.preventDefault();
+//   const {
+//     elements: { email, message }
+//   } = e.target;
+
+
+//   if (email.value === "" || message.value === "") {
+//       alert('Attention! Fill in all fields before "Submit", please ;)');
+//       return;
+//     };
+
+//     const formData = new FormData(e.target);
+//     formData.forEach((value, textContent) => {
+//         console.log(textContent, value);
+//     });  
+//   e.target.reset();
+// }
 
