ffcd93d053f71dc9ecbe7ad82e32dd919f7bd51c
pnyekrasov <126099923+pnyekrasov@users.noreply.github.com>
2023-06-18 05:55:26 +0000

    Deploying to gh-pages from @ pnyekrasov/goit-js-hw-08@1a2cd4954c401e4c55548cbd5b4e0f1f32e151aa 🚀

---
+++ 02-video.4dba1bf8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#vimeo-player");new Vimeo.Player(e)}();
+//# sourceMappingURL=02-video.4dba1bf8.js.map
---
+++ 02-video.a7819b63.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#vimeo-player");new Vimeo.Player(e);
+//# sourceMappingURL=02-video.a7819b63.js.map
