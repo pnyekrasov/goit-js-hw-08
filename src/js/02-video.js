1a2cd4954c401e4c55548cbd5b4e0f1f32e151aa
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-18 08:54:55 +0300

    HW 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Vimeo.Player(iframe);
 
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+
+
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector('#vimeo-player');
+  
+// const player = new Player('iframe', {
+
+    
+// });
+
+    // const player = new Vimeo.Player(iframe);
+
+// const onPlay = function(data) {
+//    player
+// };
+
+//     player.on('play', onPlay);
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
