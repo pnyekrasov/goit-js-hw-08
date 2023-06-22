e22cc2f87688c8610fbb1b1213980cb81b3038fd
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-22 13:40:07 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,34 @@
 import throttle from "lodash.throttle"; 
 const formEl = document.querySelector('.feedback-form');
-const formData = {};
+const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+
 formEl.addEventListener('input', throttle(handleWriteReport, 500));
 function handleWriteReport(e) {
     formData[e.target.name] = e.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }; 
 
-function recordFormData() {
-   email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
-    message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
-};
+    const {
+            elements: { email, message }
+    } = formEl;
+    const saveData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (saveData) {
+    email.value = saveData.email || '';
+    message.value = saveData.message || '';  
+    }
+;
 
     formEl.addEventListener('submit', handleDisplayReport);
-    function handleDisplayReport() {
-        const {
-            elements: { email, message }
-        } = formEl;        
-        if (email.value === "" || message.value === "") {
+    function handleDisplayReport(e) {
+               
+        if (formEl.email.value === "" || formEl.message.value === "") {
             alert('Attention! Fill in all fields before "Submit", please ;)');
             return;
         };
-       recordFormData(); 
+       
         localStorage.removeItem("feedback-form-state");
         console.log(formData);        
         formEl.reset();
+        formData = {};
     };
 
